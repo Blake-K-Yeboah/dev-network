a56fd50f05abf2868aabfa3f475aa173c39ca7f5
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-25 13:38:29 +1000

    Updated Validation and Added Login Validation

--- validation/register.js
+++ validation/register.js
@@ -1,34 +1,38 @@
 // Export Validation Function
-module.exports = validateRegisterInput = (data) => {
+module.exports = function validateRegisterInput(data) {
 
     // Define Errors
     let errors = {}
 
     // Check Different Properties
-    if (!data.firstname || data.firstname === '') {
+    if (!data.firstname) {
         errors.firstname = "First name field is required";
 
     }
 
-    if (!data.lastname || data.lastname === '') {
+    if (!data.lastname) {
         errors.lastname = "Last name field is requried";
+    }
+
+    if (!data.username) {
+        errors.username = "Username field is required";
     }
 
     if (!data.email || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)) {
         errors.email = "Email is not valid";
     }
 
-    if (!data.password || data.password === '') {
+    if (!data.password) {
         errors.password = "Password field is required";
     }
 
-    if (data.password.legnth >= 8 && data.password.length <= 30) {
+    if (data.password && !data.password.length >= 8 && !data.password.length <= 30) {
         errors.password = "Password must be at least 8 characters";
     }
 
     // Return errors and isValid Boolean
     return {
         errors,
-        isValid: errors === {} ? true : false
+        isValid: !Object.keys(errors).length
     }
 }
