ff31893aac39ab1f5fd3c8dc0fb791494964037f
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-25 13:39:03 +1000

    Updated Config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://dbAdmin:dbAdminPassword@cluster0-gol29.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://dbAdmin:dbAdminPassword@cluster0-gol29.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
