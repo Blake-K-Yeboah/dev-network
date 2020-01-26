6d6a2fc9adfbc6fd9d43de2875f57c807d253ef8
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-26 13:52:19 +1000

    Setup Basic Routing and Mobx Provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// Import Style
 import './App.css';
+
+// Import Stores
+import { authStore } from './stores/authStore';
+
+// Import Provider
+import { Provider } from 'mobx-react';
+
+// Import React Router Dom Stuff
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+// Import Pages
+import Home from './components/pages/Home/Home';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Switch>
+        <Provider authStore={authStore}>
+
+          <Route exact path="/" component={Home} />
+
+        </Provider>
+
+      </Switch>
+
+    </Router>
   );
 }
 
