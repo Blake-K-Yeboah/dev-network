98cbccfd28e4610745d6db17820fd284800e5b5d
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-30 20:19:19 +1000

    Updated Links

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar: React.FC = props => {
     return (
-        <div>
-            <h1>DevNetwork</h1>
-            <ul>
-                <li>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/projects">Projects</NavLink>
-                    <NavLink to="/community">Community</NavLink>
-                    <NavLink to="/about">About</NavLink>
+        <div className="navbar">
+            <h1 className="nav-brand"><span className="bold">Dev</span>Network</h1>
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <NavLink to="/" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/projects" className="nav-link">Projects</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/community" className="nav-link">Community</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/about" className="nav-link">About</NavLink>
                 </li>
             </ul>
         </div>
