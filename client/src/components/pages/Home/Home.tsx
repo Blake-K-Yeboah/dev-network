a859d7bb4cae5798f74fcacdd18ab5ddb69b19dc
Blake Yeboah <blakekyeboah@gmail.com>
2020-01-31 21:37:11 +1000

    ...

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Navbar from '../../layout/Navbar'
 import Header from './Header'
 import Footer from '../../layout/Footer'
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return (
         <>
+
+            <Helmet>
+
+                <title>DevNetwork - Home</title>
+
+            </Helmet>
+
             <Navbar />
+
             <div className="container">
+
                 <Header />
+
                 <Footer />
+
             </div>
+
         </>
     )
 }
