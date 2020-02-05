b05fc75a8d341ef46fdb5c3b7429aec748259055
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-05 17:05:56 +1000

    Fixed Navbar Links

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -4,22 +4,25 @@ import { NavLink } from 'react-router-dom';
 
 import { GoCode } from 'react-icons/go';
 
-const Navbar: React.FC = props => {
+const Navbar = () => {
+
+    const route = window.location.pathname;
+
     return (
         <div className="navbar">
             <h1 className="nav-brand"><GoCode className="icon" /> <span className="bold">Dev</span>Network</h1>
             <ul className="nav-list">
                 <li className="nav-item">
-                    <NavLink to="/" className="nav-link">Home</NavLink>
+                    <NavLink to="/" className={`nav-link ${route === '/' ? 'active-link' : ''}`} >Home</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/projects" className="nav-link">Projects</NavLink>
+                    <NavLink to="/projects" className={`nav-link ${route === '/projects' ? 'active-link' : ''}`}>Projects</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/community" className="nav-link">Community</NavLink>
+                    <NavLink to="/community" className={`nav-link ${route === '/community' ? 'active-link' : ''}`}>Community</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/about" className="nav-link">About</NavLink>
+                    <NavLink to="/about" className={`nav-link ${route === '/about' ? 'active-link' : ''}`}>About</NavLink>
                 </li>
             </ul>
         </div>
