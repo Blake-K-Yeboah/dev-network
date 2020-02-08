a88656128aa6909371b7dd46e618636d72e4d921
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-08 20:37:47 +1000

    Added Page Titile

--- client/src/components/pages/Projects/Projects.tsx
+++ client/src/components/pages/Projects/Projects.tsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { inject, observer } from 'mobx-react';
 import Navbar from '../../layout/Navbar';
 import Footer from '../../layout/Footer';
+import { Helmet } from 'react-helmet';
 
 const Projects = inject("authStore", "projectStore")(observer(({ authStore, projectStore }) => {
 
     return (
         <>
+            <Helmet>
+
+                <title>DevNetwork - Projects</title>
+
+            </Helmet>
+
             <Navbar />
 
             <div className="container">
