b1d60945873eaa506004caca87808b3892d64bf8
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-10 11:25:44 +1000

    Added Login Success Message

---
+++ client/src/components/pages/Login/LoginSuccess.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginSuccess = () => {
+    return (
+        <div className="success">
+            <p>You successfully signed up.</p>
+        </div>
+    )
+}
+
+export default LoginSuccess
