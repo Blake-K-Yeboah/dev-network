dc8b6392688907e3bf6b5150e17cd08f8fe1ad56
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-11 18:35:24 +1000

    Added type checking

--- client/src/stores/authStore.ts
+++ client/src/stores/authStore.ts
@@ -8,33 +8,33 @@ import { Iuser } from '../types';
 class authstore {
 
     // Store JWT Token
-    @observable token = document.cookie.split('=')[1];
+    @observable token: string = document.cookie.split('=')[1];
 
     // Store authenticated boolean
-    @observable isAuthenticated = this.token ? true : false;
+    @observable isAuthenticated: boolean = this.token ? true : false;
 
     // Store Logged in User
-    @observable user = this.token ? jwt_decode(this.token) : null;
+    @observable user: Iuser | null = this.token ? jwt_decode(this.token) : null;
 
     // Store Error
     @observable error: string | null = null;
 
     // Action to change error
     @action
-    setError(err: string) {
+    setError(err: string): void {
         this.error = err;
     }
 
     // Action to set current user
     @action
-    setCurrentUser(user: Iuser | null) {
+    setCurrentUser(user: Iuser | null): void {
         this.user = user;
         if (user === null) this.isAuthenticated = false;
     }
 
     // Action to set token
     @action
-    setToken(token: string) {
+    setToken(token: string): void {
         this.token = token;
         this.isAuthenticated = true;
     }
