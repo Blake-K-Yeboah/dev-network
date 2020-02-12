b050014510026e42f1d897d3fa1e9fc2512434a9
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-12 21:43:20 +1000

    Added Project Model

---
+++ models/project.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: Array,
+        requried: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Array,
+        default: []
+    },
+    dislikes: {
+        type: Array,
+        default: []
+    },
+    preview: {
+        type: String,
+        required: false
+    },
+    github: {
+        type: String,
+        required: false
+    },
+    postedBy: {
+        type: Object,
+        required: true
+    },
+    postedOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Project = mongoose.model('project', projectSchema);
