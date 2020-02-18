36352dd7ad678125ac2a9b271e83034f3a69f032
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-18 19:16:37 +1000

    Added Spinenr Component

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaSpinner } from 'react-icons/fa'
+
+const Spinner = () => {
+    return (
+        <FaSpinner className="spinner" />
+    )
+}
+
+export default Spinner
