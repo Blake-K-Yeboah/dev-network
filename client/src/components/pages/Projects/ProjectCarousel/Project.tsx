6ae20a243524d5c53868d9163afea476acb74085
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-18 20:03:45 +1000

    Updated Project Component

--- client/src/components/pages/Projects/ProjectCarousel/Project.tsx
+++ client/src/components/pages/Projects/ProjectCarousel/Project.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import Spinner from '../../../Spinner';
 import { IProject } from '../../../../types';
+import { IoIosCode, IoMdEye, IoMdThumbsDown, IoMdThumbsUp } from 'react-icons/io';
+import { NavLink } from 'react-router-dom';
 
 const Project = inject('projectStore', 'authStore')(observer(({ projectStore, authStore }) => {
 
@@ -18,6 +20,19 @@ const Project = inject('projectStore', 'authStore')(observer(({ projectStore, au
                         return <li className="tag">{tag}</li>
                     }) : <p className="no-tags">No Tags</p>}
                 </ul>
+                <div className="icons-container">
+                    <div className="icon-group left">
+                        {project.github ? <a href={project.github} title="Github"><IoIosCode className="icon" /></a> : <IoIosCode className="icon disabled" />}
+                        {project.preview ? <a href={project.preview} title="Preview"><IoMdEye className="icon" /></a> : <IoMdEye className="icon disabled" />}
+                    </div>
+                    <div className="icon-group right">
+                        <IoMdThumbsUp className={`icon ${project.likes.includes(authStore.user.id) ? 'active' : ''}`} />
+                        <IoMdThumbsDown className={`icon ${project.dislikes.includes(authStore.user.id) ? 'active' : ''}`} />
+                    </div>
+                </div>
+                <div className="project-footer">
+                    <p className="footer-text">Posted By <NavLink to={`/profile/`} className="profile-link">{/* Add Name */}Blake Yeboah</NavLink></p>
+                </div>
             </div> : <Spinner />}
         </>
     )
