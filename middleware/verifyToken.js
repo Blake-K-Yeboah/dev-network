33174b2cabe31e1fae72974f953e431cf9e507ed
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-20 19:43:37 +1000

    Updated Verify Token MW

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,6 +1,6 @@
 module.exports = function verifyToken(req, res, next) {
 
-    const token = req.headers.cookie.split('=')[1];
+    const token = req.headers.cookie ? req.headers.cookie.split('=')[1] : null;
 
     if (!token) {
         return res.json({ error: 'No Credentials Provided.' })
