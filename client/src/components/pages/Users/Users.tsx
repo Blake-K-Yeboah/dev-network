1402d72dbb87971aef56ad612f0555ebfdf9034d
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 15:44:24 +1000

    Created Users Page

--- client/src/components/pages/Users/Users.tsx
+++ client/src/components/pages/Users/Users.tsx
@@ -3,17 +3,28 @@ import { inject, observer } from 'mobx-react'
 import Footer from '../../layout/Footer'
 import Navbar from '../../layout/Navbar';
 import Container from '../../layout/Container';
+import Header from './Header';
+import Sidebar from '../Projects/Sidebar';
+import PostModal from '../Projects/PostModal';
 
-const Users = inject("usersStore")(observer(({ usersStore }) => {
+const Users = inject("usersStore", "projectStore")(observer(({ usersStore, projectStore }) => {
     return (
         <>
             <Navbar />
 
             <Container>
 
+                <Sidebar />
+
+                <Header />
+
                 <Footer />
 
             </Container>
+
+            <PostModal />
+
+            <div className={`overlay ${!projectStore.modalStatus ? 'hidden' : ''}`} onClick={() => projectStore.toggleStatus()}></div>
 
         </>
     )
