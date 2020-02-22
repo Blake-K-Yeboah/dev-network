392b38c267dd5532243355e31b6c3994174391a1
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 11:31:33 +1000

    Created Container Component

---
+++ client/src/components/layout/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container: React.FC = props => {
+    return (
+        <div className="container">
+
+            {props.children}
+
+        </div>
+    )
+}
+
+export default Container
