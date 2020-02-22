78f79b36702ad3aaf389369df379531e65433ee3
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 15:44:04 +1000

    Created Users Header

---
+++ client/src/components/pages/Users/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header projects">
+
+            <h1 className="title">Explore Users</h1>
+
+        </div>
+    )
+}
+
+export default Header
