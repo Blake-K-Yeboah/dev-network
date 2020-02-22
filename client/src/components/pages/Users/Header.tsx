a30124a164ae7cb6968578dc1693b1af2a6dd4b6
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 17:23:28 +1000

    Updated User Header

--- client/src/components/pages/Users/Header.tsx
+++ client/src/components/pages/Users/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import UserCarousel from './UserCarousel/UserCarousel'
 
 const Header = () => {
     return (
         <div className="header projects">
 
             <h1 className="title">Explore Users</h1>
+
+            <UserCarousel />
 
         </div>
     )
