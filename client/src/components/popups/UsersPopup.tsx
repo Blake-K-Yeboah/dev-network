0a38098b5333e028f5e7fcfb443e12be0296832a
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 11:11:32 +1000

    Created Users Popup Component

---
+++ client/src/components/popups/UsersPopup.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import { inject, observer } from 'mobx-react';
+import { useHistory } from 'react-router-dom';
+
+const UsersPopup = inject('authStore')(observer(({ authStore }) => {
+
+    useEffect(() => {
+        setTimeout(() => setStatus(true), 5000);
+        setTimeout(() => setStatus(false), 9000);
+    }, [authStore.isAuthenticated]);
+
+    const [status, setStatus] = useState(false);
+
+    let history = useHistory();
+
+    return (
+        <div className={`popup right ${status ? '' : 'hidden'}`}>
+            <p className="popup-text">You can also browse users on the users page.</p>
+            <button className="btn primary" onClick={() => history.push('/users')}>Let's Go!</button>
+        </div>
+    )
+}))
+
+export default UsersPopup;
