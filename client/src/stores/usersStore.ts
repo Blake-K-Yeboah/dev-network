d49fed08ed986225b6695bec0bf07d39ad13e135
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 17:22:19 +1000

    Updated Users Store

--- client/src/stores/usersStore.ts
+++ client/src/stores/usersStore.ts
@@ -5,10 +5,20 @@ import { Iuser } from '../types';
 class usersstore {
 
     @action fetchUsers() {
-        Axios.get('/api/users').then(res => this.users = res.data);
+        Axios.get('/api/users').then(res => this.users = res.data.reverse());
     }
 
     @observable users: Iuser[] | null = null;
+
+    @observable activeUserIndex: number = 0;
+
+    @action nextUser(): void {
+        usersStore.activeUserIndex += 1;
+    }
+
+    @action prevUser(): void {
+        usersStore.activeUserIndex -= 1;
+    }
 
 }
 
