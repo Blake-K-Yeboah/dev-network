8c611a2a8e74079798fec98a0316730288e1a495
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-22 11:13:16 +1000

    Added Browse Link w/Dropdown

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { NavLink } from 'react-router-dom';
 
 import { GoCode } from 'react-icons/go';
 
 const Navbar = () => {
+
+    // For Dropdown
+    const [status, setStatus] = useState(false);
 
     const route = window.location.pathname;
 
@@ -16,10 +19,17 @@ const Navbar = () => {
                     <NavLink to="/" className={`nav-link ${route === '/' ? 'active-link' : ''}`} >Home</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink to="/projects" className={`nav-link ${route === '/projects' ? 'active-link' : ''}`}>Projects</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink to="/community" className={`nav-link ${route === '/community' ? 'active-link' : ''}`}>Community</NavLink>
+                    <span className={`nav-link ${route === '/users' || route === '/projects' || route === '/community' ? 'active-link' : ''}`} onClick={() => setStatus(!status)}>Browse</span>
+
+                    <div className={`dropdown ${status ? '' : 'hidden'}`}>
+
+                        <NavLink to="/projects" className={`dropdown-link ${route === '/projects' ? 'active-link' : ''}`}>Projects</NavLink>
+
+                        <NavLink to="/users" className={`dropdown-link ${route === '/users' ? 'active-link' : ''}`}>Users</NavLink>
+
+                        <NavLink to="/community" className={`dropdown-link ${route === '/community' ? 'active-link' : ''}`}>Community</NavLink>
+
+                    </div>
                 </li>
                 <li className="nav-item">
                     <NavLink to="/about" className={`nav-link ${route === '/about' ? 'active-link' : ''}`}>About</NavLink>
