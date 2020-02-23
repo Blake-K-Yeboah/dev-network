e591a16c214f6ef048e23a8f4d2b6a3f332b9cb6
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-23 17:05:36 +1000

    Added Following FUnctionality

--- client/src/components/pages/Users/UserCarousel/User.tsx
+++ client/src/components/pages/Users/UserCarousel/User.tsx
@@ -2,12 +2,29 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { Iuser } from '../../../../types';
 import Spinner from '../../../Spinner';
+import Axios from 'axios';
 
 const User = inject("usersStore", "authStore")(observer(({ usersStore, authStore }) => {
 
     const user: Iuser | null = usersStore.users ? usersStore.users[usersStore.activeUserIndex] : null;
 
     const followCheck = user ? user.followers.includes(authStore.user.id) : false;
+
+    const followHandler = (): void => {
+        if (followCheck) {
+            Axios.post(`/api/users/${user ? user._id : ''}/unfollow`, { userId: authStore.user.id })
+                .then(res => {
+                    usersStore.fetchUsers();
+                })
+                .catch(err => console.error(err));
+        } else {
+            Axios.post(`/api/users/${user ? user._id : ''}/follow`, { userId: authStore.user.id })
+                .then(res => {
+                    usersStore.fetchUsers();
+                })
+                .catch(err => console.error(err));
+        }
+    }
 
     return (
         <>
@@ -17,8 +34,8 @@ const User = inject("usersStore", "authStore")(observer(({ usersStore, authStore
                     <img className="user-profilepic" src={`uploads/profile/${user.profileIcon}`} alt={`${user.firstname}'s Profile Icon`} />
                     <h2 className="name">{user.firstname} {user.lastname}</h2>
                     <p className="small">{user.username}</p>
-                    <p className="small">{user.followers.length} Followers</p>
-                    <button className={`btn ${followCheck ? 'following' : 'primary'}`}>{followCheck ? 'Following' : 'Follow'}</button>
+                    <p className="small">{user.followers.length} follower{user.followers.length > 1 || user.followers.length < 1 ? 's' : ''}</p>
+                    <button className={`btn ${followCheck ? 'following' : 'primary'}`} onClick={followHandler}>{followCheck ? 'Following' : 'Follow'}</button>
                 </div>
             }
         </>
