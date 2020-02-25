c6f3ec327793e847b165803aca5ff057a521e901
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-25 20:29:33 +1000

    Updated Result Item Component

--- client/src/components/pages/Users/ResultItem.tsx
+++ client/src/components/pages/Users/ResultItem.tsx
@@ -1,21 +1,48 @@
 import React from 'react'
-import { Iuser } from '../../../types'
+import Axios from 'axios';
+import { inject, observer } from 'mobx-react';
+import { useHistory } from 'react-router-dom';
 
-interface IresultProps {
-    result: Iuser
-}
+const ResultItem = inject("usersStore")(observer(({ usersStore, result, uid }) => {
 
-const ResultItem = (props: IresultProps) => {
+    let history = useHistory();
+
+    const followCheck = result ? result.followers.includes(uid) : false;
+
+    const followHandler = (): void => {
+        if (followCheck) {
+            Axios.post(`/api/users/${result ? result._id : ''}/unfollow`, { userId: uid })
+                .then(res => {
+                    usersStore.fetchUsers();
+                })
+                .catch(err => console.error(err));
+        } else {
+            Axios.post(`/api/users/${result ? result._id : ''}/follow`, { userId: uid })
+                .then(res => {
+                    usersStore.fetchUsers();
+                })
+                .catch(err => console.error(err));
+        }
+    }
+
+    const redirectToProfile = (): void => {
+        history.push(`/profile/${result.username.replace('@', '')}`)
+    }
     return (
         <div className="user-result">
-            <img src={`/uploads/profile/${props.result.profileIcon}`} alt={props.result.username} className="profile-icon" />
-            <h3 className="name">{props.result.firstname} {props.result.lastname}</h3>
-            <h6 className="username">{props.result.username}</h6>
+            <img src={`/uploads/profile/${result.profileIcon}`} alt={result.username} className="profile-icon" />
+            <h3 className="name">{result.firstname} {result.lastname}</h3>
+            <h6 className="username">{result.username}</h6>
+
             <div className="btn-group">
+                <button className="btn outline" onClick={redirectToProfile} >View Profile</button>
+
+                <button className={`btn ${followCheck ? 'following' : 'primary'}`} onClick={followHandler}>{followCheck ? 'Following' : 'Follow'}</button>
 
             </div>
+
         </div>
     )
-}
+}))
 
 export default ResultItem
