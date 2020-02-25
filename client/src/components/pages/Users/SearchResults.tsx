65a89a115c5c00256f9e9789aadacbb0b34caa7b
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-25 20:29:06 +1000

    Added Key to Result Item

--- client/src/components/pages/Users/SearchResults.tsx
+++ client/src/components/pages/Users/SearchResults.tsx
@@ -6,13 +6,14 @@ import ResultItem from './ResultItem';
 
 const SearchResults = inject("usersStore", "authStore")(observer(({ usersStore, authStore }) => {
 
-    const results: Iuser[] | null = usersStore.users && usersStore.searchQuery.startsWith('@') ? usersStore.users.filter((user: Iuser) => user.username.startsWith(usersStore.searchQuery)) : usersStore.users ? usersStore.users.filter((user: Iuser) => user.firstname.startsWith(usersStore.searchQuery)) : null;
+    const results: Iuser[] | null = usersStore.users && usersStore.searchQuery.startsWith('@') ? usersStore.users.filter((user: Iuser) => user.username.toLowerCase().startsWith(usersStore.searchQuery.toLowerCase())) : usersStore.users ? usersStore.users.filter((user: Iuser) => user.firstname.toLowerCase().startsWith(usersStore.searchQuery.toLowerCase())) : null;
 
     return (
         <div className="results">
             <h1 className="title">Results</h1>
             {results ? results.map(result => {
-                return <ResultItem result={result} />
+                if (result._id === authStore.user.id) return false
+                return <ResultItem result={result} uid={authStore.user.id} key={result._id} />
             }) : <Spinner />}
         </div>
     )
