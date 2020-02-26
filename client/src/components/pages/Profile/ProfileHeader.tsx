0493c9f6aa320122e42c527da96849af36753ba3
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-26 19:37:03 +1000

    Updated Profile Header

--- client/src/components/pages/Profile/ProfileHeader.tsx
+++ client/src/components/pages/Profile/ProfileHeader.tsx
@@ -3,6 +3,11 @@ import { inject, observer } from 'mobx-react';
 import Spinner from '../../Spinner';
 
 const ProfileHeader = inject("authStore")(observer(({ authStore, profile }) => {
+
+    const userCheck = profile && profile._id === authStore.user.id ? true : false;
+
+    const followCheck = profile ? profile.followers.includes(authStore.user.id) : false;
+
     return (
         <>
             {!profile ? <Spinner />
@@ -10,6 +15,23 @@ const ProfileHeader = inject("authStore")(observer(({ authStore, profile }) => {
                 : <div className="header profile">
 
                     <img className="profile-header" src={`/uploads/header/${profile.headerImg}`} alt={`${profile.firstname}'s Header`} />
+
+                    <img className="profile-pic" src={`/uploads/profile/${profile.profileIcon}`} alt={`${profile.firstname}'s Profile Icon`} />
+
+                    <div className="profile-info">
+
+                        <h1 className="profile-name">{profile.firstname} {profile.lastname}</h1>
+
+                        <h6 className="profile-username">{profile.username}</h6>
+
+                        <div className="btn-group">
+                            {userCheck ? '' : <button className={`btn ${followCheck ? 'following' : 'primary'}`}>{followCheck ? `Following ${profile.followers.length}` : `Follow ${profile.followers.length}`}</button>}
+
+                            {!userCheck && followCheck ? <button className="btn primary">Message</button> : ''}
+
+                            {userCheck ? <button className="btn warning">Edit Profile</button> : ''}
+                        </div>
+                    </div>
 
                 </div>}
         </>
