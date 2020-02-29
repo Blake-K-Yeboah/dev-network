6b7006409c4723f324625b52c10ddc5bad59733f
Blake Yeboah <blakekyeboah@gmail.com>
2020-02-29 18:50:45 +1000

    :tada: Added Individual Project + Next/Previous Btns

--- client/src/components/pages/Profile/ProfileSlider/Sections/Projects.tsx
+++ client/src/components/pages/Profile/ProfileSlider/Sections/Projects.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { inject, observer } from 'mobx-react'
 import { IProject } from '../../../../../types';
 import Spinner from '../../../../Spinner';
+import ProfileProject from './ProfileProject';
 
 const Projects = inject('usersStore', 'projectStore', 'authStore')(observer(({ usersStore, projectStore, authStore, profile }) => {
 
@@ -11,11 +12,24 @@ const Projects = inject('usersStore', 'projectStore', 'authStore')(observer(({ u
 
     const projects = projectStore.projects ? projectStore.projects.filter((project: IProject) => project.userId === profile._id) : null;
 
+    const [activeProject, setActiveProject] = useState(0);
     return (
         <div className={`project-section ${usersStore.activeTab !== 'project' ? 'hidden' : ''}`}>
-            {projects ? projects.map((project: IProject) => {
-                return <h1>{project.title}</h1>
-            }) : <Spinner />}
+            <button className={`small-circle-btn previous ${projects && !projects[activeProject - 1] ? 'disabled' : ''}`} onClick={() => setActiveProject(activeProject - 1)}>
+                <span className="icon">
+                    &lt;
+                </span>
+            </button>
+
+            {projects && projects.length > 0 ? <ProfileProject project={projects[activeProject]} />
+                : projects && projects.length < 1 ? <p className="no-projects">This user has no projects.</p>
+                    : <Spinner />}
+
+            <button className={`small-circle-btn next ${projects && !projects[activeProject + 1] ? 'disabled' : ''}`} onClick={() => setActiveProject(activeProject + 1)}>
+                <span className="icon">
+                    &gt;
+                </span>
+            </button>
         </div>
     )
 }));
