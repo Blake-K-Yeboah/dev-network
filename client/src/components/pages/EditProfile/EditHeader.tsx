06f3c4020c5a91e2c8873a7a848f072d9c38d6e6
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-08 20:53:21 +1000

    :bugs: Fixed Bug

--- client/src/components/pages/EditProfile/EditHeader.tsx
+++ client/src/components/pages/EditProfile/EditHeader.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 const EditHeader = () => {
     return (
         <div className="header profile">
+            
             <h1 className="title">Edit Profile</h1>
+
             <div className="edit-grid">
                 <div className="edit-info"></div>
                 <div className="edit-profile-pic"></div>
                 <div className="edit-header"></div>
             </div>
+
         </div>
     )
 }
