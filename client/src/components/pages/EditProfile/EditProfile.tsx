71be441783b307704e7877045772ed1ad4727e37
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-08 20:54:22 +1000

    :pencil2: Added Page Title

--- client/src/components/pages/EditProfile/EditProfile.tsx
+++ client/src/components/pages/EditProfile/EditProfile.tsx
@@ -5,10 +5,17 @@ import Container from '../../layout/Container'
 import Footer from '../../layout/Footer'
 import Sidebar from '../Projects/Sidebar'
 import EditHeader from './EditHeader'
+import { Helmet } from 'react-helmet';
 
 const EditProfile = inject('authStore')(observer(({ authStore }) => {
     return (
         <>
+
+            <Helmet>
+                
+                <title>DevNetwork - Editing Profile</title>
+
+            </Helmet>
 
             <Navbar />
 
