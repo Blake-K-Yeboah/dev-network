9de1e41ae8fc0dccefb6a2414b8db5c4af5441ca
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-23 21:54:46 +1000

    Added Edit Info Component

--- client/src/components/pages/EditProfile/EditHeader.tsx
+++ client/src/components/pages/EditProfile/EditHeader.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import EditInfo from '../EditProfile/Sections/EditInfo'
 
 const EditHeader = () => {
     return (
         <div className="header profile">
-            
+
             <h1 className="title">Edit Profile</h1>
 
             <div className="edit-grid">
-                <div className="edit-info"></div>
-                <div className="edit-profile-pic"></div>
-                <div className="edit-header"></div>
+                <EditInfo />
+                <div className="edit-profile-pic">
+                    <h2 className="heading">Profile Picture</h2>
+                </div>
+                <div className="edit-header">
+                    <h2 className="heading">Profile Header</h2>
+                </div>
             </div>
 
         </div>
