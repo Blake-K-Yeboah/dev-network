f7216f3cd2ee99a6dc0fb4e553510086579a4902
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-31 16:51:16 +1000

    :heavy_plus_sign: Created Header Component

---
+++ client/src/components/pages/Community/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header community">
+            <h1 className="title">Explore Posts</h1>
+        </div>
+    )
+}
+
+export default Header;
