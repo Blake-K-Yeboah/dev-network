124081ebb5923d243fb1cde239b5066c42683341
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-31 22:59:50 +1000

    :new: Updated Create A Post Btn

--- client/src/components/pages/Projects/Sidebar.tsx
+++ client/src/components/pages/Projects/Sidebar.tsx
@@ -5,7 +5,7 @@ import Axios from 'axios';
 import { FaSearch, FaUser, FaUserTimes, FaPlus } from 'react-icons/fa';
 import { IStoreProps } from '../../../types';
 
-const Sidebar = inject('authStore', 'projectStore', 'usersStore')(observer(({ authStore, projectStore, usersStore }: IStoreProps) => {
+const Sidebar = inject('authStore', 'projectStore', 'usersStore', 'communityStore')(observer(({ authStore, projectStore, usersStore, communityStore }: IStoreProps) => {
 
     const [status, setStatus] = useState(false);
 
@@ -62,7 +62,7 @@ const Sidebar = inject('authStore', 'projectStore', 'usersStore')(observer(({ au
 
             <button className={`btn outline ${hideStatus} ${route !== '/projects' ? 'hidden' : ''}`} onClick={(): void => projectStore.toggleStatus()}>Post A Project <FaPlus style={iconStyle} /></button>
 
-            <button className={`btn outline ${hideStatus} ${route !== '/community' ? 'hidden' : ''}`}>Create a Post <FaPlus style={iconStyle} /></button>
+            <button className={`btn outline ${hideStatus} ${route !== '/community' ? 'hidden' : ''}`} onClick={(): void => communityStore.toggleStatus()}>Create a Post <FaPlus style={iconStyle} /></button>
 
             <button className={`btn outline ${hideStatus} ${route !== '/users' ? 'hidden' : ''}`} onClick={(): void => usersStore.toggleSearchModalStatus()}>Search Users <FaSearch style={iconStyle} /></button>
 
