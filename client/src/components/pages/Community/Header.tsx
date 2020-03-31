db17fcd90c8fcf0fb6401db84b7310c15e2aaac8
Blake Yeboah <blakekyeboah@gmail.com>
2020-03-31 23:02:00 +1000

    :new: Updated Component

--- client/src/components/pages/Community/Header.tsx
+++ client/src/components/pages/Community/Header.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { inject, observer } from 'mobx-react';
+import { IStoreProps } from '../../../types';
+import CommunityCarousel from './CommunityCarousel/CommunityCarousel';
 
-const Header = () => {
+const Header = inject('communityStore')(observer(({ communityStore }: IStoreProps) => {
+
+    useEffect(() => {
+        communityStore.fetchPosts();
+    }, [communityStore]);
+
     return (
         <div className="header community">
+
             <h1 className="title">Explore Posts</h1>
+
+            <CommunityCarousel />
+
         </div>
     )
-}
+}))
 
 export default Header;
