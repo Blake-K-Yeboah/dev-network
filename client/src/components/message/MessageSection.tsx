4cbcd015f0317e362c18f6aa193203ce3bf8b261
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-05 22:36:41 +1000

    :email: Create Message Section Component

---
+++ client/src/components/message/MessageSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaEnvelope } from 'react-icons/fa'
+
+const MessageSection = () => {
+    return (
+        <div className="message-section">
+            <FaEnvelope className="icon" />
+            <h3 className="view-messages">View Messages</h3>
+        </div>
+    )
+}
+
+export default MessageSection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
