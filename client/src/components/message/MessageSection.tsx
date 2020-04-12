df0ac58b68f6049b41b7fa24f5d1269e2ca0b5ee
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-12 23:52:25 +1000

    :fire: Updated View Messages Functionality

--- client/src/components/message/MessageSection.tsx
+++ client/src/components/message/MessageSection.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { FaEnvelope } from 'react-icons/fa'
+import { inject, observer } from 'mobx-react'
+import { IStoreProps } from '../../types'
 
-const MessageSection = () => {
+const MessageSection = inject('messageStore')(observer(({ messageStore }: IStoreProps) => {
     return (
         <div className="message-section">
             <FaEnvelope className="icon" />
-            <h3 className="view-messages">View Messages</h3>
+            <h3 className="view-messages" onClick={() => messageStore.toggleStatus()}>View Messages</h3>
         </div>
     )
-}
+}));
 
 export default MessageSection
 
