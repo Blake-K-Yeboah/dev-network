86264376ceaa96ad9ed7d47140707ac4f12f44fd
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-13 22:16:10 +1000

    :email: Added Accordion Component

--- client/src/components/message/MessageModal.tsx
+++ client/src/components/message/MessageModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { IStoreProps } from '../../types'
+import MessageAccordion from './MessageAccordion'
 
 const MessageModal = inject('messageStore')(observer(({ messageStore }: IStoreProps) => {
     return (
@@ -8,6 +9,9 @@ const MessageModal = inject('messageStore')(observer(({ messageStore }: IStorePr
             <div className={`modal ${!messageStore.modalStatus ? 'hidden' : ''}`}>
                 <span className="close-icon" onClick={() => messageStore.toggleStatus()}>&times;</span>
                 <h1 className="title">Your Messages</h1>
+
+                <MessageAccordion />
+
             </div>
 
             <div className={`overlay ${!messageStore.modalStatus ? 'hidden' : ''}`} onClick={() => messageStore.toggleStatus()}></div>
