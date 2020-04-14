f27a33432af823c688246aa3ada73e529ac58578
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-15 01:13:13 +1000

    :arrows_counterclockwise: Added Socket Io Connection

--- client/src/components/message/MessageAccordion.tsx
+++ client/src/components/message/MessageAccordion.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { inject, observer } from 'mobx-react'
 import { IStoreProps } from '../../types'
 import Follower from './Follower/Follower'
+import io from "socket.io-client";
 
 const MessageAccordion = inject('authStore')(observer(({ authStore }: IStoreProps) => {
+
+    let socket: any = null;
+
+    const initSocketConnection = () => {
+        socket = io('http://localhost:5000/');
+    }
+
+    useEffect(() => {
+        initSocketConnection();
+    });
+
     return (
         <div className="accordion">
             {authStore.user.followers.map((followerID: string) => (
