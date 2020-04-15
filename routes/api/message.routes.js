7d8387783f0e7b836991e0516dcedfed7d298735
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-16 02:29:31 +1000

    :email: Added Post Message Route

--- routes/api/message.routes.js
+++ routes/api/message.routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const Message = require('../../models/message.model');
 
+// Import Validation Function
+const validateMessageInput = require('../../validation/message');
+
+// Verify TokenMiddle
+const verifyToken = require('../../middleware/verifyToken');
+
 const messageRouter = (io) => {
     let router = express.Router();
+
+    // Check User Token Middleware
+    router.use(verifyToken);
 
     router.get('/', (req, res) => {
         Message.find({}).then(messages => {
@@ -10,10 +19,36 @@ const messageRouter = (io) => {
         });
     });
 
+    router.post('/', (req, res) => {
+
+        // Validation
+        const { errors, isValid } = validateMessageInput(req.body);
+
+        // Check Validation
+        if (isValid === false) {
+            return res.status(400).json(errors);
+        }
+
+        const newMessage = new Message({
+            text: req.body.text,
+            from: req.body.from,
+            to: req.body.to
+        })
+
+        newMessage.save().then(message => {
+
+            io.emit('message', message);
+
+            res.json({ msg: 'success' });
+
+        }).catch(err => res.json(err));
+
+    });
+
     // Socket.io Conection
-    io.on('connection', () => {
-        console.log('a user is connected')
-    })
+    //io.on('connection', () => {
+    //console.log('a user is connected')
+    // })
 
     return router;
 }
