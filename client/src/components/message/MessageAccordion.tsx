b0f9fefaa5cc9976e79549770680281f27603c47
Blake Yeboah <blakekyeboah@gmail.com>
2020-04-16 02:32:02 +1000

    :email: Updated Message Accordion

--- client/src/components/message/MessageAccordion.tsx
+++ client/src/components/message/MessageAccordion.tsx
@@ -1,20 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { IStoreProps } from '../../types'
 import Follower from './Follower/Follower'
-import io from "socket.io-client";
 
 const MessageAccordion = inject('authStore')(observer(({ authStore }: IStoreProps) => {
-
-    let socket: any = null;
-
-    const initSocketConnection = () => {
-        socket = io('http://localhost:5000/');
-    }
-
-    useEffect(() => {
-        initSocketConnection();
-    });
 
     return (
         <div className="accordion">
